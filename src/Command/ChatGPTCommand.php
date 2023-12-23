fb5a08fc046541359a09c3f73233e92bde10d2d5
ramity <lewisclbrown@gmail.com>
2023-12-23 02:06:44 +0000

    fixed namespace

--- src/Command/ChatGPTCommand.php
+++ src/Command/ChatGPTCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Ramity\Conjure\Command;
+namespace Ramity\Bundle\ConjureBundle\Command;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
