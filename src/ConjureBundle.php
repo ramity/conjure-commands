c68b679be8620957ecb449ce1209d668e957241c
ramity <lewisclbrown@gmail.com>
2023-12-23 02:05:08 +0000

    reverted back to bundle approach

---
+++ src/ConjureBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Ramity\Bundle\ConjureBundle;
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ConjureBundle extends Bundle
+{
+    public function registerCommands(Application $application)
+    {
+        // Register your command(s) here
+        $application->add(new Command\ChatGPTCommand());
+    }
+}
